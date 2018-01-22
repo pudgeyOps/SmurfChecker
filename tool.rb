407e4893d50465e50973e13dd72900f7960255b9
Jonathan Pugh <jonathan.pugh@skybettingandgaming.com>
2018-01-22 13:48:26 +0000

    Starting to add an email function

--- tool.rb
+++ tool.rb
@@ -2,17 +2,38 @@
 require 'open-uri'
 require 'json'
 require "date"
+require 'net/smtp'
 
 def callapi(uri)
     open(uri)
   rescue => exception
-    puts "username cannot be found on EUW"
-    exit 1 
+    puts "Summoner cannot be found on EUW"
+    exit 1
+end
+
+def send_email(to,opts={})
+  opts[:server]      ||= 'localhost'
+  opts[:from]        ||= 'pudgey@localhost'
+  opts[:from_alias]  ||= 'Decay Bot'
+  opts[:subject]     ||= "Your decay status"
+  opts[:body]        ||= ""
+
+  msg = <<END_OF_MESSAGE
+From: #{opts[:from_alias]} <#{opts[:from]}>
+To: <#{to}>
+Subject: #{opts[:subject]}
+
+#{opts[:body]}
+END_OF_MESSAGE
+
+  Net::SMTP.start(opts[:server]) do |smtp|
+    smtp.send_message msg, opts[:from], to
+  end
 end
 
 
 if ARGV.empty?
-  puts 'No variables given, please provide a list of summoner names to do the lookup! eg. "ruby tool.rb username1 username2 username3"'
+  puts 'No usernames given, please provide a list of summoner names to do the lookup! eg. "ruby tool.rb username1 username2 username3"'
   exit
 end
 api_key = 'RGAPI-070a85ab-5cca-41dc-997e-40b050d32747'
@@ -42,7 +63,11 @@ ARGV.each do |summoner_name|
 
     if difference > 28
       puts "You are in decay"
+    elsif days_till_decay < 10
+      puts "You have less then 10 days till decay"
     else
       puts "You have #{days_till_decay} days left till decay"
     end
+
+  #send_email "#{email_address}", :body => "You have #{days_till_decay} till you decay"
 end
